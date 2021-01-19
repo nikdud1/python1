6f198bf61cb305c94028293493b4362660edbfb7
nikdud1 <76486880+nikdud1@users.noreply.github.com>
2021-01-19 17:10:52 +0300

    Delete lesson3_2.py

--- lesson3_2.py
+++
@@ -1,9 +0,0 @@
-def string1(firstname,secondname,year,town,email,phonenumber):
-    print("Имя:",firstname, ", Фамилия:",secondname, ", Год рождения:",year ,", Город:",town, ", Email:",email,", Номер телефона:",phonenumber)
-firstname1 = input("Введите имя ")
-secondname1 = input("Введите фамилию ")
-year1 = input("Введите год рождения ")
-town1 = input("Введите город ")
-email1 = input("Введите email ")
-phonenumber1 = input("Введите номер телефона ")
-string1(firstname1,secondname1,year1,town1,email1,phonenumber1)
